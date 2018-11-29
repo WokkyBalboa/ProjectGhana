450110f83ff253bea716eb5d44d823e123129ed8
WokkyBalboa <45458787+WokkyBalboa@users.noreply.github.com>
2018-11-29 14:22:44 +0100

    Add files via upload

---
+++ LiveUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiveUI : MonoBehaviour {
+
+    public Player player;
+    private Text UItext;
+    // Use this for initialization
+    void Start()
+    {
+        UItext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    //Displays player lives
+    void Update()
+    {
+        UItext.text = "Lives: " + player.lives;
+    }
+}
+
---
+++ UI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public Player player;
+    private Text UItext;
+	// Use this for initialization
+	void Start () {
+        UItext = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+    // Displays Diamonds
+	void Update () {
+        UItext.text = "Diamonds: " + player.diamonds;
+	}
+}
